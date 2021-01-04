e151b5e6e68626b4edc327e2d3162be1dc8aa2d9
wag-k <captenkenta@gmail.com>
2021-01-04 17:57:02 +0900

    画像を読み込んで輝度値に応じて屈折率に変換するクラスを用意したい

---
+++ refraction_loader.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 4 2021
+
+@author: Kenta Kawaguchi
+"""
+
+class RefractionLoader:
+    def __init__(self):
+        pass
+
+
+def main():
+    loader = RefractionLoader()
+
+if __name__ == "__main__":
+    main()

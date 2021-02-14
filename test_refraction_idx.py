88d29afd8723e66987603b63eb80af2efc24b3f9
wag-k <captenkenta@gmail.com>
2021-02-14 10:24:06 +0900

    feat: 画像読み込みテスト追加 refactor: RefIdxのリファクタリングに従って、テストを修正

--- test_refraction_idx.py
+++ test_refraction_idx.py
@@ -1,10 +1,11 @@
 import unittest
-from refraction_idx import RefractionIdx
+from refraction_idx import RefractionIdx, MeshNum
 
 class TestRefractionIdx(unittest.TestCase):
     def test_create_refraction_table1(self):
         ref_idx = RefractionIdx()
-        table = ref_idx.create_refraction_table(10, 10, 20)
+        mesh_num = MeshNum(10, 10, 20)
+        table = ref_idx.create_refraction_table(mesh_num)
         expected = (10, 10, 20)
         actual = table.shape
         self.assertEqual(expected, actual)
@@ -15,16 +16,20 @@ class TestRefractionIdx(unittest.TestCase):
         expected_x = 30
         expected_y = 30
         expected_z = 100
-        actual_x = loader.num_mesh_x
-        actual_y = loader.num_mesh_y
-        actual_z = loader.num_mesh_z
+        actual_x = loader.mesh_num.x
+        actual_y = loader.mesh_num.y
+        actual_z = loader.mesh_num.z
         self.assertEqual(expected_x, actual_x)
         self.assertEqual(expected_y, actual_y)
         self.assertEqual(expected_z, actual_z)
         
     def test_set_refraction_from_img(self):
         ref_idx = RefractionIdx()
-        ref_idx.set_refraction_from_img()
+        ref_idx.set_refraction_from_img("./image/test_pat.tif", 1., 1.2)
+        expected_1 = 1.2
+        expected_2 = 1.0
+        self.assertEqual(expected_1, ref_idx.table[0, 0, 0])
+        self.assertEqual(expected_2, ref_idx.table[29, 29, 99])
 
 if __name__ == "__main__":
     unittest.main()
